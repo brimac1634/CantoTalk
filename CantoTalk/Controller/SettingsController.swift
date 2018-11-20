a5e7fc69ef346d16553abcba00f0c2f9cf33d84d
Brian MacPherson <brimac1634@gmail.com>
2018-11-20 14:12:54 +0800

    Minor changes to settings sections

--- CantoTalk/Controller/SettingsController.swift
+++ CantoTalk/Controller/SettingsController.swift
@@ -8,39 +8,45 @@
 
 import UIKit
 
-class Setting: NSObject {
-    let imageName: String
-    let name: String
-    
-    init(name: String, imageName: String) {
-        self.name = name
-        self.imageName = imageName
-    }
-}
+//class Setting: NSObject {
+//    let imageName: String
+//    let name: String
+//
+//    init(name: String, imageName: String) {
+//        self.name = name
+//        self.imageName = imageName
+//    }
+//}
 
-class SettingsController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+class SettingsController: UIViewController {
 
-    let logoDiameter: CGFloat = 200
+//    let logoDiameter: CGFloat = 200
     
+//    lazy var tableView: UITableView = {
+//        let tv = UITableView()
+//        tv.backgroundColor = UIColor.cantoLightBlue(a: 1)
+//        tv.separatorStyle = .none
+//        tv.translatesAutoresizingMaskIntoConstraints = false
+//        tv.delegate = self
+//        tv.dataSource = self
+//        return tv
+//    }()
     
+//    let cellID = "cellID"
+//    let settings: [Setting] = {
+//        return [Setting(name: "Request Words", imageName: "request"), Setting(name: "Notifications", imageName: "notification"), Setting(name: "Rate Us", imageName: "rate_us")]
+//    }()
+//    var vcs: [UIViewController]?
     
-    lazy var tableView: UITableView = {
-        let tv = UITableView()
-        tv.backgroundColor = UIColor.cantoLightBlue(a: 1)
-        tv.separatorStyle = .none
-        tv.translatesAutoresizingMaskIntoConstraints = false
-        tv.delegate = self
-        tv.dataSource = self
-        return tv
+    let notificationsLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Notifications"
+        label.textColor = UIColor.cantoDarkBlue(a: 1)
+        label.font = UIFont.systemFont(ofSize: 30)
+        label.textAlignment = .center
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
     }()
-    
-    
-    
-    let cellID = "cellID"
-    let settings: [Setting] = {
-        return [Setting(name: "Request Words", imageName: "request"), Setting(name: "Notifications", imageName: "notification"), Setting(name: "Rate Us", imageName: "rate_us")]
-    }()
-    var vcs: [UIViewController]?
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -50,43 +56,46 @@ class SettingsController: UIViewController, UITableViewDataSource, UITableViewDe
 
 
     func setupViews() {
-        view.backgroundColor = UIColor.cantoLightBlue(a: 1)
+        view.backgroundColor = UIColor.cantoWhite(a: 1)
         
-
-        view.addSubview(tableView)
+        view.addSubview(notificationsLabel)
+//        view.addSubview(tableView)
         
         NSLayoutConstraint.activate([
-            
-            tableView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
-            tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
-            tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
-            tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -16)
+            notificationsLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            notificationsLabel.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 0.8),
+            notificationsLabel.topAnchor.constraint(equalTo: view.topAnchor, constant: 32),
+            notificationsLabel.heightAnchor.constraint(equalToConstant: 32)
+//            tableView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+//            tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+//            tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+//            tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -16)
             ])
 
         
-        tableView.register(SettingCell.self, forCellReuseIdentifier: cellID)
+//        tableView.register(SettingCell.self, forCellReuseIdentifier: cellID)
     }
     
 
-    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        //do something
-    }
-    
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return settings.count
-    }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: cellID, for: indexPath) as! SettingCell
-        let settings = self.settings[indexPath.row]
-        cell.accessoryType = .disclosureIndicator
-        cell.settings = settings
-        return cell
-    }
-    
-    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        return 50
-    }
+//    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+//        //do something
+//    }
+//
+//    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+//        return settings.count
+//    }
+//
+//    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+//        let cell = tableView.dequeueReusableCell(withIdentifier: cellID, for: indexPath) as! SettingCell
+//        let settings = self.settings[indexPath.row]
+//        cell.accessoryType = .disclosureIndicator
+//        cell.settings = settings
+//        return cell
+//    }
+//
+//    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+//        return 50
+//    }
     
 
 }
--- CantoTalk/View/SettingsCell.swift
+++ CantoTalk/View/SettingsCell.swift
@@ -1,65 +1,65 @@
+////
+////  SettingsCell.swift
+////  CantoTalk
+////
+////  Created by Brian MacPherson on 14/8/2018.
+////  Copyright © 2018 Brian MacPherson. All rights reserved.
+////
 //
-//  SettingsCell.swift
-//  CantoTalk
+//import UIKit
 //
-//  Created by Brian MacPherson on 14/8/2018.
-//  Copyright © 2018 Brian MacPherson. All rights reserved.
+//class SettingCell: BaseTableViewCell {
 //
-
-import UIKit
-
-class SettingCell: BaseTableViewCell {
-    
-    override var isHighlighted: Bool {
-        didSet {
-            backgroundColor = isHighlighted ? UIColor.cantoDarkBlue(a: 1) : UIColor.cantoWhite(a: 1)
-            cellTitleLabel.textColor = isHighlighted ? UIColor.cantoPink(a: 1) : UIColor.cantoDarkBlue(a: 1)
-            cellImage.tintColor = isHighlighted ? UIColor.cantoPink(a: 1) : UIColor.cantoDarkBlue(a: 1)
-        }
-    }
-    
-    var settings: Setting? {
-        didSet {
-            cellTitleLabel.text = settings?.name
-            if let image = settings?.imageName {
-                cellImage.image = UIImage(named: image)?.withRenderingMode(.alwaysTemplate)
-                cellImage.tintColor = UIColor.cantoDarkBlue(a: 1)
-            }
-            
-        }
-    }
-    
-    let cellImage: UIImageView = {
-        let image = UIImageView()
-        image.image = UIImage(named: "notification")?.withRenderingMode(.alwaysTemplate)
-        image.contentMode = .scaleAspectFill
-        image.translatesAutoresizingMaskIntoConstraints = false
-        return image
-    }()
-    
-    let cellTitleLabel: UILabel = {
-        let label = UILabel()
-        label.textColor = UIColor.cantoDarkBlue(a: 1)
-        label.font = UIFont.systemFont(ofSize: 13)
-        label.translatesAutoresizingMaskIntoConstraints = false
-        return label
-    }()
-    
-    
-    override func setupViews() {
-        addSubview(cellImage)
-        addSubview(cellTitleLabel)
-        
-        NSLayoutConstraint.activate([
-            cellImage.widthAnchor.constraint(equalToConstant: 30),
-            cellImage.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 8),
-            cellImage.centerYAnchor.constraint(equalTo: centerYAnchor),
-            
-            cellTitleLabel.leadingAnchor.constraint(equalTo: cellImage.trailingAnchor, constant: 16),
-            cellTitleLabel.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -8),
-            cellTitleLabel.centerYAnchor.constraint(equalTo: centerYAnchor)
-            ])
-        
-    }
-    
-}
+//    override var isHighlighted: Bool {
+//        didSet {
+//            backgroundColor = isHighlighted ? UIColor.cantoDarkBlue(a: 1) : UIColor.cantoWhite(a: 1)
+//            cellTitleLabel.textColor = isHighlighted ? UIColor.cantoPink(a: 1) : UIColor.cantoDarkBlue(a: 1)
+//            cellImage.tintColor = isHighlighted ? UIColor.cantoPink(a: 1) : UIColor.cantoDarkBlue(a: 1)
+//        }
+//    }
+//
+//    var settings: Setting? {
+//        didSet {
+//            cellTitleLabel.text = settings?.name
+//            if let image = settings?.imageName {
+//                cellImage.image = UIImage(named: image)?.withRenderingMode(.alwaysTemplate)
+//                cellImage.tintColor = UIColor.cantoDarkBlue(a: 1)
+//            }
+//
+//        }
+//    }
+//
+//    let cellImage: UIImageView = {
+//        let image = UIImageView()
+//        image.image = UIImage(named: "notification")?.withRenderingMode(.alwaysTemplate)
+//        image.contentMode = .scaleAspectFill
+//        image.translatesAutoresizingMaskIntoConstraints = false
+//        return image
+//    }()
+//
+//    let cellTitleLabel: UILabel = {
+//        let label = UILabel()
+//        label.textColor = UIColor.cantoDarkBlue(a: 1)
+//        label.font = UIFont.systemFont(ofSize: 13)
+//        label.translatesAutoresizingMaskIntoConstraints = false
+//        return label
+//    }()
+//
+//
+//    override func setupViews() {
+//        addSubview(cellImage)
+//        addSubview(cellTitleLabel)
+//
+//        NSLayoutConstraint.activate([
+//            cellImage.widthAnchor.constraint(equalToConstant: 30),
+//            cellImage.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 8),
+//            cellImage.centerYAnchor.constraint(equalTo: centerYAnchor),
+//
+//            cellTitleLabel.leadingAnchor.constraint(equalTo: cellImage.trailingAnchor, constant: 16),
+//            cellTitleLabel.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -8),
+//            cellTitleLabel.centerYAnchor.constraint(equalTo: centerYAnchor)
+//            ])
+//
+//    }
+//
+//}
